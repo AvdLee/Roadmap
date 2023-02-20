194000059149ba9ace85ea9cc6b0ddd5e95ab80f
Hidde van der Ploeg <hiddevanderploeg@me.com>
2023-02-20 15:26:16 +0100

    Update RoadmapStyle.swift

--- Sources/Roadmap/Styling/RoadmapStyle.swift
+++ Sources/Roadmap/Styling/RoadmapStyle.swift
@@ -9,10 +9,19 @@ import Foundation
 import SwiftUI
 
 public struct RoadmapStyle {
+    /// The image used for the upvote button
     let upvoteIcon : Image
+    
+    /// The font used for the feature
     let titleFont : Font
+    
+    /// The font used for the count label
     let numberFont : Font
+    
+    /// The font used for the status views
     let statusFont : Font
+    
+    /// The corner radius for the upvote button
     let radius : CGFloat
     
     public init(icon: Image, titleFont: Font, numberFont: Font, statusFont: Font, cornerRadius: CGFloat) {
