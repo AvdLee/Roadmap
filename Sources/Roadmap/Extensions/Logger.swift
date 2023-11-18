0003269d53cbeb111ee356e2087469af5aa28b03
MaartenBorsje <59605923+MaartenBorsje@users.noreply.github.com>
2023-11-18 15:34:17 +0100

    Update Logger.swift

--- Sources/Roadmap/Extensions/Logger.swift
+++ Sources/Roadmap/Extensions/Logger.swift
@@ -13,8 +13,5 @@ extension Logger {
     private static var subsystem = Bundle.main.bundleIdentifier!
 
     /// Logs the view cycles like a view that appeared.
-    static let viewCycle = Logger(subsystem: subsystem, category: "viewcycle")
-
-    /// All logs related to tracking and analytics.
-    static let statistics = Logger(subsystem: subsystem, category: "statistics")
+    static let roadmap = Logger(subsystem: subsystem, category: "Roadmap")
 }
